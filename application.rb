cf5ff8a5f2ef175a814ef77928814d6addd9abeb
mguetlein <martin.guetlein@gmail.com>
2010-06-28 18:17:49 +0200

    move to wrapper version 1.5.7 (application/x-yaml)

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-gem 'opentox-ruby-api-wrapper', '= 1.5.6'
+gem 'opentox-ruby-api-wrapper', '= 1.5.7'
 require 'opentox-ruby-api-wrapper'
 
 LOGGER.progname = File.expand_path(__FILE__)
